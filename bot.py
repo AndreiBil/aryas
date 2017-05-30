ae9baba2bcf38fb35912767e0a028c797540e4af
Your Name <bileca.andrei@gmail.com>
2017-05-31 02:21:52 +0300

    Added purge command , deleted other irrelevant commands

--- bot.py
+++ bot.py
@@ -34,33 +34,12 @@ async def clear(ctx, number):
             counter += 1
             await asyncio.sleep(0.1) #1.2 second timer so the deleting process can be even
 
-@bot.command()
-async def add(left : int, right : int):
-    """Adds two numbers together."""
-    await bot.say(left + right)
-
-@bot.command()
-async def roll(dice : str):
-    """Rolls a dice in NdN format."""
-    print(dice)
-    try:
-        rolls, limit = map(int, dice.split('d'))
-    except Exception:
-        await bot.say('Format has to be in NdN!')
-        return
-
-    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
-    await bot.say(result)
 
 @bot.command(pass_context=True)
 async def ping(ctx):
     await bot.say('Pong!')
 
-@bot.command()
-async def repeat(times : int, content='repeating...'):
-    """Repeats a message multiple times."""
-    for i in range(times):
-        await bot.say(content)
+
 
 
 
