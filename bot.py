20a33ffe6e67817f04083779604c3c509ce518f3
Your Name <bileca.andrei@gmail.com>
2017-05-31 01:21:09 +0300

    1st commit to the aryas bot

---
+++ bot.py
@@ -0,0 +1,49 @@
+import discord
+
+from discord.ext import commands
+import random
+
+description = "Aryas-Bot"
+bot_prefix = '?'
+
+bot = commands.Bot(command_prefix='?', description=description)
+
+
+
+
+
+@bot.event
+async def on_ready():
+    print('Logged in')
+    print('Name : {}'.format(bot.user.name))
+    print('ID : {}'.format(bot.user.id))
+    print(discord.__version__)
+
+
+@bot.command()
+async def add(left : int, right : int):
+    """Adds two numbers together."""
+    await bot.say(left + right)
+
+@bot.command()
+async def roll(dice : str):
+    """Rolls a dice in NdN format."""
+    print(dice)
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await bot.say('Format has to be in NdN!')
+        return
+
+    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    await bot.say(result)
+
+@bot.command(pass_context=True)
+async def ping(ctx):
+    await bot.say('Pong!')
+
+@bot.command()
+async def repeat(times : int, content='repeating...'):
+    """Repeats a message multiple times."""
+    for i in range(times):
+        await bot.say(content)
