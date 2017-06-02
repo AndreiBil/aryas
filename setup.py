08ecc9b343a0750d8cd93c2273575677641b0fe2
AFodil <fodil.aissa@etu.univ-amu.fr>
2017-06-03 00:50:28 +0200

    Made setup.py easier to use (stdin) And made it a bit more reusable

--- setup.py
+++ setup.py
@@ -1,32 +1,27 @@
+import json
 import sqlite3
 import sys
 from collections import defaultdict
-import json
 
 
 def main(argv):
+    """
+    Will create a love table, and a json file
+    :param argv:    the args
+        -> None: will use stdin
+        -> discord_token: the discord token that will be put in the json file
+    """
     arg_dict = build_arg_list(argv)
 
     if 'discord_token' in arg_dict:
-        # Create a SQLite DB and connect to it.
-        conn = sqlite3.connect('aryas.db')
-        c = conn.cursor()
-        # Create love table
-        c.execute("""CREATE TABLE IF NOT EXISTS love 
-                      (giver CHAR(18), receiver CHAR(18), channel CHAR(18), server CHAR(18), amount INTEGER)""")
-
-        # Create secrets.json file. Declared object inline for brevity.
-        json_data = json.dumps({
-            'discord': {
-                'token': arg_dict['discord_token'][0]
-            }
-        }, sort_keys=True, indent=2, separators=(',', ': '))
-
-        # Write json_data to the secrets.json file.
-        with open('secrets.json', 'w') as outfile:
-            outfile.write(json_data)
+        # args
+        cred = arg_dict['discord_token'][0]
     else:
-        return print('Please supply a discord_token')
+        # stdin
+        cred = input('Type the cred: ')
+
+    create_love_table()
+    create_secret_json(cred)
 
 
 def build_arg_list(argv):
@@ -40,6 +35,35 @@ def build_arg_list(argv):
         d[k].append(v)
 
     return dict(d)
+
+
+def create_secret_json(token):
+    """
+        Create secrets.json file.
+        :param token: the token of the bot to add in the json
+    """
+    json_data = json.dumps({
+        'discord': {
+            'token': token
+        }
+    }, sort_keys=True, indent=2, separators=(',', ': '))
+
+    # Write json_data to the secrets.json file.
+    with open('secrets.json', 'w') as outfile:
+        outfile.write(json_data)
+
+
+def create_love_table():
+    """
+    Creates a love table on a SQLite DB
+    """
+    # Create a SQLite DB and connect to it.
+    conn = sqlite3.connect('aryas.db')
+    c = conn.cursor()
+    # Create love table
+    c.execute("""CREATE TABLE IF NOT EXISTS love
+                  (giver CHAR(18), receiver CHAR(18), channel CHAR(18), server CHAR(18), amount INTEGER)""")
+
 
 if __name__ == '__main__':
     main(sys.argv[1:])
