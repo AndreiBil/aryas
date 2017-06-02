9c780c089d32a478d47cd1835927f687efb3fc56
Tom Sherman <the.tomsherman@gmail.com>
2017-06-02 21:46:55 +0100

    Amended build argument dictionary function

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import json
 
 
 def main(argv):
-    arg_dict = build_arg_list(argv)
+    arg_dict = build_arg_dict(argv)
 
     if 'discord_token' in arg_dict:
         # Create a SQLite DB and connect to it.
@@ -29,17 +29,49 @@ def main(argv):
         return print('Please supply a discord_token')
 
 
-def build_arg_list(argv):
+def build_arg_dict(argv):
     """
     Takes a list of system arguments and transforms them into a dictionary.
-    :param argv: ie. sys.argv[1:]
-    :return d:  The argument dictionary
+    :param argv: A tuple to 
+    :return:  The argument dictionary
     """
     d = defaultdict(list)
-    for k, v in ((k.lstrip('-'), v) for k, v in (a.split('=') for a in argv)):
-        d[k].append(v)
+
+    for index, arg in enumerate(argv):
+        if index < len(argv) - 1:
+            current_arg, next_arg = arg, argv[index+1]
+        else:
+            current_arg, next_arg = arg, None
+
+        new_key = None
+
+        # Double hyphen, equals
+        if current_arg.startswith('--') and '=' in current_arg:
+            new_key, val = current_arg.split('=')
+
+        # Double hyphen, no equals
+        # Single hyphen, no arg
+        elif (current_arg.startswith('--') and '=' not in current_arg) or \
+                (current_arg.startswith('-') and (not next_arg or next_arg.startswith('-'))):
+            val = True
+
+        # Single hyphen, arg
+        elif current_arg.startswith('-') and next_arg and not next_arg.startswith('-'):
+            val = next_arg
+
+        else:
+            if (next_arg is None) or (current_arg == val):
+                continue
+
+            else:
+                raise ValueError('Unexpected argument pair: %s, %s' % (current_arg, next_arg))
+
+        # Sanitize the key
+        key = (new_key or current_arg).strip(' -')
+        d[key].append(val)
 
     return dict(d)
+
 
 if __name__ == '__main__':
     main(sys.argv[1:])
