ca52b9539927923f360235b5b0d585860e079b2c
Tom Sherman <the.tomsherman@gmail.com>
2017-06-03 12:57:20 +0100

    Add ban command (#10)

--- src/extensions/modtools.py
+++ src/extensions/modtools.py
@@ -27,6 +27,23 @@ class ModTools:
 
     @commands.command(pass_context=True)
     @commands.has_role('Admin')
+    async def ban(self, ctx, member: discord.Member, reason):
+        """
+        Bans the mentioned user from the server
+        :param ctx: The message context
+        :param member: The member to be banned
+        :param reason: The reason for the ban
+        """
+        try:
+            await self.bot.ban(member)
+            msg = '{} was banned by {}. Reason: {}'.format(member.name, ctx.message.author.mention, reason)
+            await send(self.bot, msg, get_channel_by_name(ctx.message.server, MOD_LOG_CHANNEL_NAME))
+        except Exception as e:
+            print(e)
+            await send(self.bot, 'Failed to ban ' + member.mention, ctx.message.channel, True)
+
+    @commands.command(pass_context=True)
+    @commands.has_role('Admin')
     async def clear(self, ctx, number):
         """
         Purges messages from the channel
