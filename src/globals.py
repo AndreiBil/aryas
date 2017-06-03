a2408fd9382d3c816edec963c0396efc05755dba
Johan de Jager <johan.dejager@live.nl>
2017-06-03 14:29:14 +0200

    Update globals.py (#11)
    Changed some double quotes to single quotes

--- src/globals.py
+++ src/globals.py
@@ -7,7 +7,7 @@ with open('secrets.json') as data_file:
     SECRETS = json.load(data_file)
 
 # The possible status an User can have
-possible_status = {'away', "vacation", "active"}
+possible_status = {'away', 'vacation', 'active'}
 
 
 MESSAGE_SLEEP_TIME = 2
