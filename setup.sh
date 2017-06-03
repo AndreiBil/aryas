e79b01ac5319adc26f8504fe1b9048119740cd78
Nat Karmios <nat@karmios.com>
2017-06-04 00:31:34 +0100

    Fixed install script error.

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-if [[ -z "${1+present}" ]]; then
-    (>&2 echo "BUILD FAILED: Please provide a Discord token.")
+if [[ -z "${2+present}" ]]; then
+    (>&2 echo "BUILD FAILED: Please provide a Discord token and weater API key.")
 else
-    docker build . --build-arg DISCORD_TOKEN=$1 WEATHER_KEY=$2 -t aryas_bot
+    docker build . --build-arg DISCORD_TOKEN=$1 --build-arg WEATHER_KEY=$2 -t aryas_bot
 fi
 
 
