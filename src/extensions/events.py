e0136386cb3e751cc2b5af3cf4026d6cd988c1ea
BlackTiger1500 <alihasnain09@hotmail.com>
2017-06-05 14:17:10 -0400

    Added doc strings and comments
    Doc strings and comments are added to explain the anti-advertisement process

--- src/extensions/events.py
+++ src/extensions/events.py
@@ -10,19 +10,19 @@ class Events:
   @commands.event
   async def on_message(self, message):
     """
-    Checks for Discord server links in a message
-    then kicks members if links are found
+    Checks for Discord server links in a message then
+    kicks the member if links are found in their message
     """
     if message.author != self.bot.user:
       if 'Moderator' not in message.author.roles:
         pattern = 'discord.gg/[a-z0-9A-Z]+'
-         if re.search(pattern, message.content):
-            reason = 'Advertising without permission'
-            await self.bot.delete_message(message)
-            await self.bot.send_message(message.author, 'Advertising is not allowed without permission!')
-            await kick_user(message.author.name, self.bot.user.mention, message.server, self.bot, reason)
+        if re.search(pattern, message.content): #Uses the Discord server link pattern to look for links in a message using the re module
+          reason = 'Advertising without permission'
+          await self.bot.delete_message(message)
+          await self.bot.send_message(message.author, 'Advertising is not allowed without permission!')
+          await kick_user(message.author, self.bot.user, message.server, self.bot, reason)
     
-    await self.bot.process_commands(message)
+    await self.bot.process_commands(message) #This is needed to process commands since the 'on_message' event blocks commands
 
 def setup(bot):
     bot.add_cog(Events(bot))
