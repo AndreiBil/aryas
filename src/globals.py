c49d606d16720ee7221239a77e08a1e8ffb4ec6e
Nat Karmios <NatKarmios@users.noreply.github.com>
2017-06-07 20:56:06 +0100

    Fix clear() command, add fancy countdown to send() helper function. (#76)
    * Added a visual countdown to auto-deleted messages.
    
    * Added an error utility function to src/utility.py
    
    This will send a PM to the user with an optional prefix, and an optional error message.
    
    * Fixed error() bug
    
    The error message wasn't being added to the full message because of a little oversight :P
    
    * Refactored error() to command_error()
    
    This makes the command's purpose a tad clearer.
    
    * Added a fancy countdown to auto-deleted messages
    
    * Fixed clear() command
    
    As per infraGem's request.
    
    * Fixed command_error to use clean_content instead of raw content
    
    * Updated fancy countdown to use • instead of .
    
    Ooh, pretty!

--- src/globals.py
+++ src/globals.py
@@ -38,7 +38,7 @@ def your_awesome_func():
     return 1+1
 ```"""
 
-MESSAGE_SLEEP_TIME = 2
+MESSAGE_SLEEP_TIME = 3
 MOD_LOG_CHANNEL_NAME = 'mod_log'
 LEN_UNITS = {'millimeter': 0.001, 'mm': 0.001, 'centimeter': 0.01, 'cm': 0.01, 'meter': 1, 'm': 1, 'kilometer': 1000,
              'km': 1000, 'inch': 0.0254, 'in': 0.0254, 'foot': 3.28084, 'ft': 3.28084, 'yard': 1.09361}
--- src/utility.py
+++ src/utility.py
@@ -39,19 +39,34 @@ def get_channel_by_name(server: Server, name: str) -> Union[Channel, None]:
     return None
 
 
-async def send(bot: commands.Bot, message: str, channel: Channel, delete=False, time=MESSAGE_SLEEP_TIME) -> None:
+async def send(bot: commands.Bot, message: str, channel: Channel, delete=False, time=MESSAGE_SLEEP_TIME,
+               show_dots=True, bomb_themed_dots=False) -> None:
     """
     Sends a message to the server and deletes it after a period of time
     :param bot:     the bot used to send the message
     :param message: the content of the message
     :param channel: the channel in which the message will be sent
-    :param delete: bool indicating whether to delete the message after sending it
+    :param delete: whether to delete the message after sending it
     :param time: the time to wait before deleting the message
+    :param show_dots: whether to show countdown dots for message deletion (this will round down `time` if it is a float)
+    :param bomb_themed_dots: whether to theme the dots using a bomb and fuse instead of plain dots
     """
-    msg = await bot.send_message(channel, message)
+
+    def dot_bar(progress):
+        width = int(time)
+        if bomb_themed_dots:
+            return "\n`💣" + "-"*(width-progress) + "*`" if width-progress > 0 else "💥"
+        return "\n`|" + "•"*(width-progress) + " "*max(progress, 0) + "|`"
+
+    msg = await bot.send_message(channel, message + (dot_bar(0) if show_dots else ""))
     # Waits *time* seconds and deletes the confirmation message.
     if delete:
-        await asyncio.sleep(time)
+        if not show_dots:
+            await asyncio.sleep(time)
+        else:
+            for i in range(int(time)):
+                await asyncio.sleep(1)
+                await bot.edit_message(msg, message+dot_bar(i+1))
         await bot.delete_message(msg)
 
 async def command_error(ctx: commands.Context, msg=None, prefix=True):
