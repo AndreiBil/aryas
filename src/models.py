e6933cae8544853099f80823de4ee762d581779d
Nat <nat@karmios.com>
2017-06-09 13:53:31 +0100

    Revamped models.py and aryasorm.py to handle config injection
    EXTREME reflection!

--- src/models.py
+++ src/models.py
@@ -1,117 +1,130 @@
-from peewee import *
 import datetime
-from src.extensions.config import Config
+from typing import Tuple
 
-_config = Config()
-database_proxy = _config.db['database_proxy']
+from peewee import SqliteDatabase, MySQLDatabase, Model, PrimaryKeyField, DateTimeField, FixedCharField, CharField, \
+    BooleanField, TextField, IntegerField, ForeignKeyField, CompositeKey, Proxy
 
-# Based on the `env` config variable, use a different database provider
-if _config.env == 'dev':
-    database = SqliteDatabase(_config.cache_dir + _config.db['name'] + '.db')
-elif _config.env == 'prod':
-    database = MySQLDatabase(
-        database=_config.db['name'],
-        user=_config.db['user'],
-        password=_config.db['pass'],
-        host=_config.db['host'],
-        charset='utf8mb4'
-    )
-else:
-    raise Exception('{} is not a valid environment.'.format(_config.env))
+from src.extensions.config import Config as _Config
 
 
-class BaseModel(Model):
-    """
-    The base model
-    """
-    # All models have an auto incrementing integer primary key.
-    id = PrimaryKeyField()
-    created_at = DateTimeField(default=datetime.datetime.now)
-    updated_at = DateTimeField()
-
-    def save(self, *args, **kwargs):
-        self.updated_at = datetime.datetime.now()
-        return super(BaseModel, self).save(*args, **kwargs)
-
-    class Meta:
-        # Uses the global database currently. Will use env vars to potentially differentiate between different
-        # environments in the future eg. production env uses MySQL, dev env uses SQLite
-        database = database_proxy
+class Models:
+    def __init__(self, base_model, discord_model, user, server, channel, message, server_nick, love_transaction):
+        self.BaseModel = base_model
+        self.DiscordModel = discord_model
+        self.User = user
+        self.Server = server
+        self.Channel = channel
+        self.Message = message
+        self.ServerNick = server_nick
+        self.LoveTransaction = love_transaction
 
 
-class DiscordModel(BaseModel):
-    """
-    Applies to everything that directly models a discord object eg. User, Server, Channel, Message
-    """
-    discord_id = FixedCharField(18, index=True, unique=True)
+# noinspection PyClassHasNoInit
+def get_models(config: _Config) -> Tuple[Proxy, Models]:
 
+    db_cfg = config.db
+    database_proxy = db_cfg["_db_proxy"]
 
-class User(DiscordModel):
-    """
-    Models a Discord user.
-    Whereas the discord.py library has sub classes such as Member, this is not relevant in db-land.
-    """
-    name = CharField(default='')
-    game = CharField(default='')
-    status = CharField(default='')
-    discriminator = CharField(default='')
-    top_role = CharField(default='')
-    is_bot = BooleanField(default=False)
-    notes = TextField(default='')
-    # TODO: make this autoincrement
-    total_messages = IntegerField(default=0)
+    # Based on the `env` config variable, use a different database provider
+    if config.env == 'dev':
+        database_ = SqliteDatabase(config.constants.cache_dir + config.db['name'] + '.db')
+    elif config.env == 'prod':
+        database_ = MySQLDatabase(
+            database=db_cfg['name'],
+            user=db_cfg['user'],
+            password=db_cfg['pass'],
+            host=db_cfg['host'],
+            charset='utf8mb4'
+        )
+    else:
+        raise Exception('{} is not a valid environment.'.format(config.env))
 
-    @property
-    def total_love(self):
-        total = 0
-        for love in LoveTransaction.select().where(LoveTransaction.receiver == self):
-            total += love.amount
-        return total
+    class BaseModel(Model):
+        """
+        The base model
+        """
+        # All models have an auto incrementing integer primary key.
+        id = PrimaryKeyField()
+        created_at = DateTimeField(default=datetime.datetime.now)
+        updated_at = DateTimeField()
 
+        def save(self, *args, **kwargs):
+            self.updated_at = datetime.datetime.now()
+            return super(BaseModel, self).save(*args, **kwargs)
 
-class Server(DiscordModel):
-    """
-    Models a Discord Server.
-    """
-    pass
+        class Meta:
+            database = database_proxy
 
+    class DiscordModel(BaseModel):
+        """
+        Applies to everything that directly models a discord object eg. User, Server, Channel, Message
+        """
+        discord_id = FixedCharField(18, index=True, unique=True)
 
-class Channel(DiscordModel):
-    """
-    Models a Discord channel.
-    """
-    server = ForeignKeyField(Server, related_name='channels')
+    class User(DiscordModel):
+        """
+        Models a Discord user.
+        Whereas the discord.py library has sub classes such as Member, this is not relevant in db-land.
+        """
+        name = CharField(default='')
+        game = CharField(default='')
+        status = CharField(default='')
+        discriminator = CharField(default='')
+        top_role = CharField(default='')
+        is_bot = BooleanField(default=False)
+        notes = TextField(default='')
+        # TODO: make this autoincrement
+        total_messages = IntegerField(default=0)
 
+        @property
+        def total_love(self):
+            total = 0
+            for love in LoveTransaction.select().where(LoveTransaction.receiver == self):
+                total += love.amount
+            return total
 
-class Message(DiscordModel):
-    """
-    Models a Discord message.
-    """
-    user = ForeignKeyField(User, related_name='messages')
-    channel = ForeignKeyField(Channel, related_name='messages')
-    body = TextField()
-    edited = DateTimeField(null=True)
-    is_command = BooleanField(default=False)
+    class Server(DiscordModel):
+        """
+        Models a Discord Server.
+        """
+        pass
 
+    class Channel(DiscordModel):
+        """
+        Models a Discord channel.
+        """
+        server = ForeignKeyField(Server, related_name='channels')
 
-class ServerNick(BaseModel):
-    """
-    Stores the relationship between servers and users' nicknames.
-    """
-    user = ForeignKeyField(User)
-    server = ForeignKeyField(Server)
+    class Message(DiscordModel):
+        """
+        Models a Discord message.
+        """
+        user = ForeignKeyField(User, related_name='messages')
+        channel = ForeignKeyField(Channel, related_name='messages')
+        body = TextField()
+        edited = DateTimeField(null=True)
+        is_command = BooleanField(default=False)
 
-    class Meta:
-        primary_key = CompositeKey('user', 'server')
+    class ServerNick(BaseModel):
+        """
+        Stores the relationship between servers and users' nicknames.
+        """
+        user = ForeignKeyField(User)
+        server = ForeignKeyField(Server)
 
+        class Meta:
+            primary_key = CompositeKey('user', 'server')
 
-class LoveTransaction(BaseModel):
-    """
-    Models a love transaction. When one user shows love to another, this is a transaction.
-    """
-    amount = IntegerField()
-    giver = ForeignKeyField(User, related_name='love_givers', index=True)
-    receiver = ForeignKeyField(User, related_name='love_receivers', index=True)
-    channel = ForeignKeyField(Channel)
+    class LoveTransaction(BaseModel):
+        """
+        Models a love transaction. When one user shows love to another, this is a transaction.
+        """
+        amount = IntegerField()
+        giver = ForeignKeyField(User, related_name='love_givers', index=True)
+        receiver = ForeignKeyField(User, related_name='love_receivers', index=True)
+        channel = ForeignKeyField(Channel)
 
-database_proxy.initialize(database)
+    models = Models(BaseModel, DiscordModel, User, Server, Channel, Message, ServerNick, LoveTransaction)
+
+    database_proxy.initialize(database_)
+    return database_proxy, models
