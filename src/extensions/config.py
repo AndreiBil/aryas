f21a2b011b88bd4aa30e716a76626d54313c5924
SeriousHamburger <hamhork@gmail.com>
2017-06-14 15:51:23 -0400

    Added Google Search

--- src/extensions/config.py
+++ src/extensions/config.py
@@ -40,6 +40,11 @@ class _Constants:
             }},
             'weather': {'type': 'dict', 'required': False, 'default': default['weather'], 'schema': {
                 'api_key': {'type': 'string', 'required': False, 'default': default['weather']['api_key']}
+            }},
+            'google': {'type': 'dict', 'required': True, 'default': default['google'], 'schema': {
+                'api_key': {'type': 'string', 'required': False, 'default': default['google']['api_key']},
+                'cse_name': {'type': 'string', 'required': False, 'default': default['weather']['cse_name']},
+                'cse_id': {'type': 'string', 'required': False, 'default': default['weather']['api_key']}                
             }}
         }
 
@@ -51,7 +56,7 @@ class _Constants:
 4. Try Google before you ask questions here
 5. Don't advertise any other discord server in any of the channels/DMs unless given permission by a mod/admin.
 6. Use the appropriate channels for your question.
-7. Don't have anything relevant to add in the programming channels? Stay quiet :smiley: :kissing_heart: 
+7. Don't have anything relevant to add in the programming channels? Stay quiet :smiley: :kissing_heart:
 8. Don't post any NSFW content.
 9.  Use your common sense ¯\_(ツ)_/¯
 10. Don't ask people for help in PMs. Use the designated channels instead.
