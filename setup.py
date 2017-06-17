b4e95a022e1507eb0b9f6d6e935171d65aca72fa
Nat Karmios <nat@karmios.com>
2017-06-17 11:43:18 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+import re
+
+
+def parse_requirements(file_name):
+    requirements = []
+    for line in open(file_name, 'r').read().split('\n'):
+        if re.match(r'(\s*#)|(\s*$)', line):
+            continue
+        if re.match(r'\s*-e\s+', line):
+            requirements.append(re.sub(r'\s*-e\s+.*#egg=(.*)$', r'\1', line))
+        elif re.match(r'\s*-f\s+', line):
+            pass
+        else:
+            requirements.append(line)
+
+setup(
+    name="AryasBot",
+    version="0.1",
+    packages=find_packages(exclude="tests"),
+    long_description=open('README.md').read(),
+    install_requires=parse_requirements("requirements.txt"),
+    entry_points={'console_scripts': ['aryas=aryasbot.__main__:main']},
+    scripts=['bin/aryas'],
+)
