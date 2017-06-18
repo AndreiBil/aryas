5731dbfef0a89bb547f345f57d53676342412e8b
Nat Karmios <nat@karmios.com>
2017-06-18 23:23:50 +0100

    Added nicer formatting to installation output
    MOAR professionalism!

--- install_docker.sh
+++ install_docker.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-echo "=== ARYAS INSTALLER (with Docker) ==="
+echo && echo "=== ARYAS INSTALLER (with Docker) ==="
 sudo echo > /dev/null # Get sudo permissions
 
 command -v docker >/dev/null 2>&1 || { echo >&2 "Docker is not installed! Aborting!"; exit 1; }
 
-sudo docker build . -t aryas \
-  && sudo cp ./bin/run_docker.sh /usr/local/bin/aryas \
+echo && echo "Building Docker image:" \
+  && sudo docker build . -t aryas | sed 's/^/    /' \
+  && echo && echo "Copying executable script to '/usr/local/bin'..." \
+  && sudo cp ./bin/run_docker.sh /usr/local/bin/aryas | sed 's/^/    /' \
   && echo && echo "Installed! Use 'aryas' to start, or 'aryas -d' to start in the background."
--- install_nodocker.sh
+++ install_nodocker.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-echo "=== ARYAS INSTALLER (without Docker) ==="
+echo && echo "=== ARYAS INSTALLER (without Docker) ==="
 sudo echo > /dev/null # Get sudo permissions
 
-echo "Installing Python requirements:" \
-  && sudo -H pip3 install -r ./requirements.txt \
+echo && echo "Installing Python requirements:" \
+  && sudo -H pip3 install -r ./requirements.txt | sed 's/^/    /' \
   && echo && echo "Installing Aryas:" \
-  && sudo python3 ./setup.py install \
+  && sudo python3 ./setup.py install | sed 's/^/    /' \
   && echo && echo "Installed! Use 'aryas' to start."
